dac1f0de3c6cd75cf37223ac5b88ab913afc73ea
Zijun-Ning <96545885+Zijun-Ning@users.noreply.github.com>
2022-07-23 11:17:25 -0700

    Update perusall_bot.py

--- perusall_bot.py
+++ perusall_bot.py
@@ -8,7 +8,7 @@ import pyperclip
 
 ##### USER INPUT AREA #####
 retina_display = True              # Usually ture if you are using Apple computer/laptop
-read_speed = 0                     # adjust this number to read faster or slower, recommend between 0 and 5
+read_speed = 0                     # adjust this number to read faster or slower
 ###########################
 
 time.sleep(5)
@@ -16,11 +16,11 @@ pyautogui.PAUSE = 0.01
 screenWidth, screenHeight = pyautogui.size()
 
 #locate important things on the screen
-taskbar_location = pyautogui.locateOnScreen('task_bar.png', confidence=0.85)
-comment_location = pyautogui.locateOnScreen('comment_section.png', confidence=0.85)
-perusall_big_location = pyautogui.locateOnScreen('perusall_big.png', confidence=0.85)
-side_bar_location = pyautogui.locateOnScreen('side_bar.png', confidence=0.85)
-conv_location = pyautogui.locateOnScreen('conv.png')
+taskbar_location = pyautogui.locateOnScreen('Resources//task_bar.png', confidence=0.85)
+comment_location = pyautogui.locateOnScreen('Resources//comment_section.png', confidence=0.85)
+perusall_big_location = pyautogui.locateOnScreen('Resources//perusall_big.png', confidence=0.85)
+side_bar_location = pyautogui.locateOnScreen('Resources//side_bar.png', confidence=0.85)
+conv_location = pyautogui.locateOnScreen('Resources//conv.png')
 next_x = 0
 next_y = 0
 
@@ -75,12 +75,12 @@ def find_first_conv():
     pyautogui.click()
     pyautogui.move(-40, 0)
 
-    while pyautogui.locateOnScreen('next.png', region=(comment_location)) is None:
+    while pyautogui.locateOnScreen('Resources//next.png', region=(comment_location)) is None:
         pyautogui.move(0, 10)
         pyautogui.click()
     pyautogui.click()
     global next_x, next_y
-    next_pos = pyautogui.locateCenterOnScreen('next.png', region=(comment_location))
+    next_pos = pyautogui.locateCenterOnScreen('Resources//next.png', region=(comment_location))
     next_x = next_pos.x
     next_y = next_pos.y
 
@@ -98,21 +98,21 @@ def next_conv():
 #           comment_loaction IS A VALID region
 #EFFECTS:   CLICK ON ALL THE LIKES CURRENTLY ON SCREEN
 def like_in_conv():
-    green_likes = pyautogui.locateAllOnScreen('green_like.png', region=(comment_location))
+    green_likes = pyautogui.locateAllOnScreen('Resources//green_like.png', region=(comment_location))
     if green_likes != None:
         for pos in green_likes:
             if retina_display:
                 pyautogui.click(pos.left / 2, pos.top / 2)
             else:
                 pyautogui.click(pos.left, pos.top)
-    likes = pyautogui.locateAllOnScreen('like.png', region=(comment_location))
+    likes = pyautogui.locateAllOnScreen('Resources//like.png', region=(comment_location))
     if likes != None:
         for pos in likes:
             if retina_display: 
                 pyautogui.click(pos.left / 2, pos.top / 2)
             else:
                 pyautogui.click(pos.left, pos.top)
-    questions = pyautogui.locateAllOnScreen('question.png', region=(comment_location))
+    questions = pyautogui.locateAllOnScreen('Resources//question.png', region=(comment_location))
     if questions != None:
         for pos in questions:
             if retina_display: 
@@ -127,43 +127,43 @@ def like_in_conv():
 #           RETURN THE SUMMARY AS A string
 def summarize():
     text = img_to_str(pyautogui.screenshot(region=(ss_area)))
-    summary_location = pyautogui.locateCenterOnScreen('summary.png', region=(taskbar_location))
+    summary_location = pyautogui.locateCenterOnScreen('Resources//summary.png', region=(taskbar_location))
     if retina_display:
         pyautogui.click(summary_location.x / 2, summary_location.y / 2)
-        paste_location = pyautogui.locateCenterOnScreen('summary_paste.png')
+        paste_location = pyautogui.locateCenterOnScreen('Resources//summary_paste.png')
         if paste_location != None:
             pyautogui.click(paste_location.x / 2, paste_location.y / 2)
     else:
         pyautogui.click(summary_location.x, summary_location.y)
-        if(pyautogui.locateCenterOnScreen('summary_paste.png') != None):
-            pyautogui.click('summary_paste.png')
+        if(pyautogui.locateCenterOnScreen('Resources//summary_paste.png') != None):
+            pyautogui.click('Resources/summary_paste.png')
     process = text.replace("\n", " ")
     pyautogui.write(process)
 
-    summarize_location = pyautogui.locateCenterOnScreen('summarize.png')
+    summarize_location = pyautogui.locateCenterOnScreen('Resources//summarize.png')
     if retina_display:
         pyautogui.click(summarize_location.x / 2, summarize_location.y / 2 )
         time.sleep(5)
-        copy_location = pyautogui.locateCenterOnScreen('copy.png')
+        copy_location = pyautogui.locateCenterOnScreen('Resources//copy.png')
         pyautogui.click(copy_location.x / 2, copy_location.y / 2)
 
         pyautogui.click(screenWidth / 3, screenHeight / 2)
         pyautogui.hotkey('command', 'a')
         pyautogui.press('backspace')
 
-        perusall_location = pyautogui.locateCenterOnScreen('Perusall.png')
+        perusall_location = pyautogui.locateCenterOnScreen('Resources//Perusall.png')
         pyautogui.click(perusall_location.x / 2, perusall_location.y / 2)
         
     else:
         pyautogui.click(summarize_location.x, summarize_location.y)
         time.sleep(2)
-        pyautogui.click('copy.png')
+        pyautogui.click('Resources//copy.png')
         
         pyautogui.click(screenWidth / 3, screenHeight / 2)
         pyautogui.hotkey('ctrl', 'a')
         pyautogui.press('backspace')
         
-        pyautogui.click('Perusall.png')
+        pyautogui.click('Resources//Perusall.png')
 
     return pyperclip.paste()
 
@@ -173,16 +173,16 @@ def summarize():
 #EFFECTS:   WRITE sum_str INTO THE COMMENT BOX IN PERUSALL
 def write_summary(sum_str):
     if retina_display:
-        pyautogui.moveTo((comment_location[0] + comment_location[2]/2) / 2, (comment_location[1] + comment_location[3] / 2)/2)
+        pyautogui.moveTo((comment_location[0] + comment_location[2] / 2) / 2, (comment_location[1] + comment_location[3]) / 2)
         pyautogui.scroll(-100 * 100)
         time.sleep(2)
-        comment_box = pyautogui.locateCenterOnScreen('comment.png', region= comment_location)
+        comment_box = pyautogui.locateCenterOnScreen('Resources//comment.png', region= comment_location)
         pyautogui.click(comment_box.x / 2, comment_box.y / 2)
     else:
         pyautogui.moveTo(comment_location[0] + comment_location[2] / 2, comment_location[1] + comment_location[3])
         pyautogui.scroll(-100 * 100)
         time.sleep(2)
-        pyautogui.click('comment.png')
+        pyautogui.click('Resources//comment.png')
     pyautogui.click()
     pyautogui.write(sum_str)        
     pyautogui.press('enter')
@@ -199,9 +199,9 @@ def write_summary(sum_str):
 #           THE DOCUMENT BY CLICKING THE next BUTTON
 def main():
 
-    while not is_done():
-        print("not done yet")
-        time.sleep(read_speed * 30)
+    # while not is_done():
+    #     print("not done yet")
+    #     time.sleep(read_speed * 30)
 
     find_first_conv()
     while not is_done():
